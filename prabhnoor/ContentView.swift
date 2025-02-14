4b06a3da8d0f3e571f2bf1d297d23acc389b9835
Prabhs920 <prabhnoors920@gmail.com>
2025-02-13 23:01:22 -0500

    Add instruction text asking if the number is prime.

--- prabhnoor/ContentView.swift
+++ prabhnoor/ContentView.swift
@@ -65,4 +65,29 @@ var body: some View {
         }
     }
 }
+var body: some View {
+    ZStack {
+        LinearGradient(gradient: Gradient(colors: [Color.purple.opacity(0.6), Color.blue.opacity(0.6)]), startPoint: .top, endPoint: .bottom)
+            .edgesIgnoringSafeArea(.all)
+
+        VStack(spacing: 40) {
+            Text("Prime Number Game")
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundColor(.white)
+                .shadow(radius: 5)
+
+            Text("\(number)")
+                .font(.system(size: 100, weight: .heavy, design: .rounded))
+                .foregroundColor(.white)
+                .shadow(radius: 5)
+
+            // Instruction Text
+            Text("Is this number Prime?")
+                .font(.title3)
+                .foregroundColor(.white)
+                .opacity(0.8)
+        }
+    }
+}
 

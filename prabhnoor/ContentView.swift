9a2db61f67f2d188f3e0c6b4c00dabb98cb17382
Prabhs920 <prabhnoors920@gmail.com>
2025-02-13 23:00:13 -0500

    Display random number on the screen.

--- prabhnoor/ContentView.swift
+++ prabhnoor/ContentView.swift
@@ -45,3 +45,24 @@ var body: some View {
         }
     }
 }
+var body: some View {
+    ZStack {
+        LinearGradient(gradient: Gradient(colors: [Color.purple.opacity(0.6), Color.blue.opacity(0.6)]), startPoint: .top, endPoint: .bottom)
+            .edgesIgnoringSafeArea(.all)
+
+        VStack(spacing: 40) {
+            Text("Prime Number Game")
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundColor(.white)
+                .shadow(radius: 5)
+
+            // Displaying the number
+            Text("\(number)")
+                .font(.system(size: 100, weight: .heavy, design: .rounded))
+                .foregroundColor(.white)
+                .shadow(radius: 5)
+        }
+    }
+}
+
